8ce851886da506e5318aa0c01085fd46f9421741
Abdelkrim <Abdelkrim-Dev@Outlook.Com>
2014-07-25 18:57:18 +0200

    Homework done

--- Banker.Domain.Test/BankerTest.cs
+++ Banker.Domain.Test/BankerTest.cs
@@ -50,18 +50,13 @@ namespace Banker.Domain.Test
         {
             //تجهيز
             var accRepoFake = A.Fake<IAccountsRepository>();
-            var account = new Account();
-            A.CallTo(() =>
-                accRepoFake.Get(1))
-                .Returns(account);
-
             var transRepoFake = A.Fake<ITransactionsRepository>();
             var sut = new AccountantService(accRepoFake, transRepoFake);
             //تنفيذ
             var result = sut.Deposit(1, 1000);
             //تحقق
             A.CallTo(() =>
-               accRepoFake.Update(account)).MustHaveHappened(Repeated.Exactly.Once);
+               transRepoFake.Create(1,1000)).MustHaveHappened(Repeated.Exactly.Once);
         }
     }
 }
